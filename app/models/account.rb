92fe61dfa041d699e033e0ab78c6aa7442d58f36
Florian Kapfenberger <florian.kapfenberger@me.com>
2016-12-28 20:31:12 +0100

    removed Base64, because 'doveadm pw -s SHA512-CRYPT' doesn't neither

--- app/models/account.rb
+++ app/models/account.rb
@@ -15,30 +15,20 @@ class Account < ApplicationRecord
     "#{self.username}@#{self.domain}"
   end
 
-  def self.hash_password password
-    salt = SecureRandom.hex(32)
-    hash = Base64.strict_encode64(password.crypt('$6$' + salt))
-    "{SHA512-CRYPT}#{hash}"
+  def salt
+    SecureRandom.hex(32)
   end
 
   def hash_password
-    salt = SecureRandom.hex(32)
-    hash = Base64.strict_encode64(self.password.crypt('$6$' + salt))
+    hash = self.password.crypt('$6$' + salt)
     self.password = "{SHA512-CRYPT}#{hash}"
   end
 
-  def hash_password_with_given password, salt
-    hash = Base64.strict_encode64(password.crypt('$6$' + salt))
-    password = "{SHA512-CRYPT}#{hash}"
-  end
-
-  def decoded
-    Base64.strict_decode64(self.password.to_s.gsub(/^{SHA512-CRYPT}/, ''))
-  end
-
   def check_password? password
-    salt = decoded.split('$').third.to_s
-    password.crypt('$6$' + salt) == decoded
+    hash = self.password.to_s.gsub(/^{SHA512-CRYPT}/, '')
+    salt = hash.split('$')[2]
+
+    password.crypt('$6$' + salt) == hash
   end
 
   def clear_password
