0f8c28fa7b56f31a9d9e11283fdc7ac13565d262
Florian Kapfenberger <florian@kapfenberger.me>
2019-08-28 11:18:51 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const db = knex({
     host: process.env.MAILMAN_DB_HOST || "127.0.0.1",
     user: process.env.MAILMAN_DB_USER,
     password: process.env.MAILMAN_DB_PASSWORD,
-    database: process.env.MAILMAN_DB_DATABASE
+    db: process.env.MAILMAN_DB_DATABASE
   }
 });
 
